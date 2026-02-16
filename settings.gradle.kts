f6f34d79a61d17572790b4d8efa9d01740bd69ff
Claw <claw@claw001>
2026-02-16 09:53:48 -0500

    Fix settings.gradle.kts: use dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,12 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
